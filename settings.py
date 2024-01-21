6e660500ca141acb1ab1f04b1f01def93164a5cf
Rishthegod <rishsharma420@gmail.com>
2024-01-20 16:46:32 -0800

    Install requirements for the project and initialize settings for the project, including API key and query url

---
+++ settings.py
@@ -0,0 +1,6 @@
+SEARCH_KEY = "AIzaSyCOtPTOmIlLjRe59dPATSgl9BlSDHRTOw4"
+SEARCH_ID = "33d1f8743a47f476d"
+COUNTRY = "us"
+
+SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&gl=" + COUNTRY
+RESULT_COUNT = 20
